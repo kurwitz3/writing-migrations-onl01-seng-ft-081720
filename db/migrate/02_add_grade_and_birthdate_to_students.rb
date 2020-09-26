e8329c0212977f7debf0e196beb55931764d64ff
Richard Kurwitz <kurwitz3@aol.com>
2020-09-26 00:13:20 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,15 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
    def change 
+<<<<<<< HEAD
      add_column  :students,:grade, :string  
     add_column  :students, :birthdate, :integer  
   
+=======
+     add_column  do |c|
+       c.integer :grade 
+       c.string :birthdate 
+   end 
+>>>>>>> d166b7e7b6efd9aa08b46dd699f2b0513b12fc75
   end
 end 
--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change 
    change_column :students, :birthdate, :datetime
+=======
+ class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+  
+  def change 
+    change_column :students  do |c|
+      c.datetime :birthdate
+
+>>>>>>> d166b7e7b6efd9aa08b46dd699f2b0513b12fc75
 end
 end 
